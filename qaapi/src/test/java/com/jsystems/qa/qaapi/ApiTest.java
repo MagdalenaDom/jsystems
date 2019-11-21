7e7e97f47c76d6ea8d5b92a6fc7d5d2ea19b36ed
MagdalenaDom <magda.nomada@gmail.com>
2019-11-21 14:54:22 +0100

    Api Tests, Postman, Hamcrest

---
+++ qaapi/src/test/java/com/jsystems/qa/qaapi/ApiTest.java
@@ -0,0 +1,24 @@
+package com.jsystems.qa.qaapi;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+
+@Tag("ApiTest")
+public class ApiTest {
+
+    @Test
+    public void firstApiTest(){
+
+        RestAssured
+                .given()
+                .get("http://www.mocky.io/v2/5a6b69ec3100009d211b8aeb")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body("name", equalTo("Piotr"))
+                .body("surname", equalTo("Kowalski"));
+    }
+}
